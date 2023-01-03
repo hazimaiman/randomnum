761655f70acdd42f84d9bd85566b91d0072e6fa1
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-03 15:46:01 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+
+            int num1 = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
+            // if want double num
+            //double num = random.NextDouble();
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
